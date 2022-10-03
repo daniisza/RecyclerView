ef817af6a9b30f1da438e0403c7f4f450906659d
NelsonMontoya <andresito.asb@gmail.com>
2022-10-03 17:31:03 -0500

    Versión Dos

--- app/src/main/java/com/movilespascual/recyclerview/MainActivity.java
+++ app/src/main/java/com/movilespascual/recyclerview/MainActivity.java
@@ -24,14 +24,8 @@ public class MainActivity extends AppCompatActivity {
 
     public void  init(){
         elementos = new ArrayList<>();
-        elementos.add(new ListaElementos("#7B1FA2","Pedro","Ciudad de México","Activo"));
-        elementos.add(new ListaElementos("#D500F9","Julio","Bogotá","Activo"));
-        elementos.add(new ListaElementos("#880E4F","Juan","Cali","Cancelado"));
-        elementos.add(new ListaElementos("#F9A825","Jessica","Paris","Inactivo"));
-        elementos.add(new ListaElementos("#689F38","Amalia","Ciudad de México","Activo"));
-        elementos.add(new ListaElementos("#0091EA","Katie De Felice","New York","Activo"));
-        elementos.add(new ListaElementos("#F06292","Evelin","New York","Inactiva"));
-        elementos.add(new ListaElementos("#00E5FF","Jean Paul","Marsella","Activo"));
+        //elementos.add(new ListaElementos("#7B1FA2","Pedro","Ciudad de México","Activo"));
+        
 
         ListAdapter listAdapter = new ListAdapter(elementos,this);
         RecyclerView recyclerView = findViewById(R.id.listRecyclerView);
